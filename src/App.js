caad5aed88fe5ece164a5fd576b22f142414b937
pggggggggh <pggggggggh@yonsei.ac.kr>
2023-05-29 00:37:42 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import React from "react";
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="header">
+        <div id="header-area"></div>
+      </div>
+      <div id="body">
+        <Routes>
+          <Route exact={true} path={"/"} element={<>kmk</>} />
+          <Route exact={true} path={"/upload"} element={<>kmk</>} />
+        </Routes>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
